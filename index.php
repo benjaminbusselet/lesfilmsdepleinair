a8498429c8e1bbd0c76ef3aedcab5599767ee8c2
benb781 <48715350+benb781@users.noreply.github.com>
2020-03-11 14:51:10 +0100

    initial

---
+++ index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="cache-control" content="no-cache" />
+
+    <title>Les Films de Plein Air</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-brand">Les Films de Plein Air</div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#association">Notre Association</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#programmation">La Programmation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#parc">Le Parc Monceau</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#inscrire">S'inscrire</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+    <header class="container-fluid">
+        <div class="row">
+            <div class="offset-lg-3 col-lg-6">
+                <h3>Du 5 au 8 août</h3>
+                <h2>Festival des Films de Plein Air</h2>
+                <p class="textfestival">Situé dans le cadre magnifique du Parc Monceau à Paris. Nous organisons ce festival afin de faire découvrir ou redécouvrir les plus beaux films d'auteurs.
+                    <br>
+                    <br> Il se déroulera du 5 au 8 aout. 2 films seront projetés par soirée. La première séance sera à 18h, la deuxième à 21h.</p>
+                <h3>Parc Monceau - Paris</h3>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
+    </header>
+
+    <div class="container-fluid">
+        <section class="row" id="association">
+            <div class="offset-lg-4 col-lg-4 text-center">
+                <h4>Notre Association</h4>
+                <hr>
+            </div>
+            <div class="col-lg-6 text-center">
+                <h1>Les Films de Plein Air</h1>
+                <img class="img-fluid" src="assets/images/jenniferviala.jpg" alt="Jennifer Viala">
+                <br>
+                <h6>Jennifer Viala - Organisatrice</h6>
+                <p>Amoureux du cinéma, nous souhaitons faire partager notre passion avec le plus grand nombre.</p>
+            </div>
+
+            <div class="col-lg-6 text-center" id="twitter">
+                <a class="twitter-timeline" data-width="auto" data-height="500" data-theme="dark" href="https://twitter.com/JenniferViala?ref_src=twsrc%5Etfw">Tweets by JenniferViala</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+        </section>
+    </div>
+
+    <section class="container-fluid" id="programmation">
+        <div class="row">
+            <div class="offset-lg-4 col-lg-4 text-center">
+                <h4>La programmation</h4>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="offset-2 col-8 text-center">
+                <button class="btn btn-link" data-target="#main-carousel" data-slide-to="0" type="button">Le 5 Aout</button>
+                <button class="btn btn-link" data-target="#main-carousel" data-slide-to="1" type="button">Le 6 Aout</button>
+                <button class="btn btn-link" data-target="#main-carousel" data-slide-to="2" type="button">Le 7 Aout</button>
+                <button class="btn btn-link" data-target="#main-carousel" data-slide-to="3" type="button">Le 8 Aout</button>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div id="main-carousel" class="carousel slide">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="img-fluid" src="assets/images/soiree1.png" alt="Le 5 Aout">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="img-fluid" src="assets/images/soiree2.png" alt="Le 6 Aout">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="img-fluid" src="assets/images/soiree3.png" alt="Le 7 Aout">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="img-fluid" src="assets/images/soiree4.png" alt="Le 8 Aout">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="parc">
+        <div class="row">
+            <div class="offset-lg-4 col-lg-4 text-center">
+                <h4>Le Parc Monceau</h4>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <p class="textparc">La construction du parc Monceau remonte au XVIIe siècle, sous l’impulsion du duc de Chartres. Situé dans le 8e arrondissement, il est aujourd’hui l’un des plus élégants jardins de Paris, à l’image du quartier. Le visiteur pénètre par les grandes grilles en fer forgé rehaussées d’or. La promenade réserve de belles surprises : nombreuses statues, une arcade Renaissance de l’ancien Hôtel de Ville de Paris, arbres spectaculaires, grande diversité d’oiseaux et un grand bassin. Le parc Monceau est entouré d’immeubles de luxe et de somptueux hôtels particuliers, dont le musée Cernuschi (arts asiatiques). Un parc calme et agréable fréquenté par les parisiens et les touristes. Des aires de jeux pour les enfants sont disponibles.</p>
+            </div>
+            <div class="col-lg-6" id="map">
+                <img class="img-fluid" src="assets/images/map.png">
+                <p class="map"><strong>Metro</strong> : Monceau - Ligne 2<br>
+                    <br>
+                    <strong>Vélib</strong> : Station N° 17018, 4 rue de thann - Station N° 8036, 39 rue de lisbonne - Station N° 8044, 2 rue alfred de vigny<br><br>
+                    <strong>Accès Handicapé</strong> : Entrée de plain-pied boulevard de Courcelles et avenue Valesquez. Les toilettes ne sont pas accessibles (5 marches).</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid" id="inscrire">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="offset-lg-4 col-lg-4 text-center">
+                    <h4>S'inscrire</h4>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="offset-lg-4 col-lg-4">
+                    <h6>L’entrée est totalement gratuite. Nous vous demanderons juste de vous inscrire sur notre site.</h6>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="offset-lg-4 col-lg-4">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Nom">
+                            <br>
+                            <input type="text" class="form-control" placeholder="Prénom">
+                            <br>
+                            <input type="email" class="form-control" placeholder="Email">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="offset-lg-4 col-lg-4">
+                    <h6>Nombre de places pour chaque soir :</h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="offset-lg-4 col-lg-1">
+                    <div class="form-group">
+                        <label for="Select">Le 5 Août</label>
+                        <select class="form-control">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-lg-1">
+                    <div class="form-group">
+                        <label for="Select">Le 6 Août</label>
+                        <select class="form-control">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-lg-1">
+                    <div class="form-group">
+                        <label for="Select">Le 7 Août</label>
+                        <select class="form-control">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-lg-1">
+                    <div class="form-group">
+                        <label for="Select">Le 8 Août</label>
+                        <select class="form-control">
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="offset-lg-4 col-lg-4">
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-link">Valider</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 text-center">
+                <h6>© 2018 Les Films de Plein Air</h6>
+            </div>
+            <div class="col-lg-6 text-center">
+                <h6>Made by Ben</p>
+            </div>
+            <div class="col-lg-3 text-center">
+                <h6>ALL RIGHTS RESERVED</h6>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
